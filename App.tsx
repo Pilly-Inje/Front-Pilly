52a04da156d3aebe4f221837d7a95d5dc87bcae0
최경영 <yeong7098@naver.com>
2025-02-26 17:10:20 +0900

    Init

---
+++ App.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+
+
+function App(): React.JSX.Element {
+  return (
+    <SafeAreaView style ={styles.safeArea}>
+      <View style={styles.container}>
+        <Text>main</Text>   
+      </View>
+    </SafeAreaView>
+    
+  );
+}
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1, 
+    backgroundColor: 'white',
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
+
+
+export default App;
